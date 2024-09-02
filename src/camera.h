410c7c61f97de82a63ed6579c81399d658ee9fc8
Kapilarny <kapilarnyyt@gmail.com>
2024-09-02 18:32:40 +0200

    sum updates

--- src/camera.h
+++ src/camera.h
@@ -1,21 +1,12 @@
 // Stolen from LearnOpenGL (https://learnopengl.com/Getting-started/Camera)
 
-#ifndef CAMERA_H
-#define CAMERA_H
+#pragma once
 
 // #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT
-};
-
-// Default camera values
+// Config camera settings, with defaults.
 static inline float YAW         = -90.0f;
 static inline float PITCH       =  0.0f;
 static inline float SPEED       =  2.5f;
@@ -24,39 +15,27 @@ static inline float ZOOM        =  45.0f;
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
-class Camera
-{
+class Camera {
 public:
-    // camera Attributes
+    // Camera attributes
     glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
     glm::vec3 Right;
     glm::vec3 WorldUp;
-    // euler Angles
-    float Yaw;
-    float Pitch;
-    // camera options
-    float MovementSpeed;
-    float MouseSensitivity;
-    float Zoom;
 
     // constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f)) : Front(glm::vec3(0.0f, 0.0f, -1.0f))
     {
         Position = position;
         WorldUp = up;
-        Yaw = yaw;
-        Pitch = pitch;
         updateCameraVectors();
     }
     // constructor with scalar values
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ) : Front(glm::vec3(0.0f, 0.0f, -1.0f))
     {
         Position = glm::vec3(posX, posY, posZ);
         WorldUp = glm::vec3(upX, upY, upZ);
-        Yaw = yaw;
-        Pitch = pitch;
         updateCameraVectors();
     }
 
@@ -66,50 +45,48 @@ public:
         return glm::lookAt(Position, Position + Front, Up);
     }
 
-    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
-    {
-        float velocity = MovementSpeed * deltaTime;
-        if (direction == FORWARD)
-            Position += Front * velocity;
-        if (direction == BACKWARD)
-            Position -= Front * velocity;
-        if (direction == LEFT)
-            Position -= Right * velocity;
-        if (direction == RIGHT)
-            Position += Right * velocity;
+    void ChangeX(int direction) {
+        Position += Right * SPEED * (float)direction;
+    }
+    void ChangeY(int direction) {
+        Position.y += SPEED * (float)direction;
+    }
+    void ChangeZ(int direction) {
+        Position += Front * SPEED * (float)direction;
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
     void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true)
     {
-        xoffset *= MouseSensitivity;
-        yoffset *= MouseSensitivity;
+        xoffset *= SENSITIVITY;
+        yoffset *= SENSITIVITY;
 
-        Yaw   += xoffset;
-        Pitch += yoffset;
+        YAW   += xoffset;
+        PITCH += yoffset;
 
-        // make sure that when pitch is out of bounds, screen doesn't get flipped
-        if (constrainPitch)
-        {
-            if (Pitch > 89.0f)
-                Pitch = 89.0f;
-            if (Pitch < -89.0f)
-                Pitch = -89.0f;
-        }
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped.
+            float upper_bound = 89.0f;
+            float lower_bound = -upper_bound;
+            
+            if (constrainPitch) {
+                if (PITCH > upper_bound)
+                    PITCH = upper_bound;
+                if (PITCH < lower_bound)
+                    PITCH = lower_bound;
+            }
 
-        // update Front, Right and Up Vectors using the updated Euler angles
+        // Update Front, Right and Up Vectors using the updated Euler angles.
         updateCameraVectors();
     }
 
     // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
     void ProcessMouseScroll(float yoffset)
     {
-        Zoom -= (float)yoffset;
-        if (Zoom < 1.0f)
-            Zoom = 1.0f;
-        if (Zoom > 45.0f)
-            Zoom = 45.0f;
+        ZOOM -= (float)yoffset;
+        if (ZOOM < 1.0f)
+            ZOOM = 1.0f;
+        if (ZOOM > 45.0f)
+            ZOOM = 45.0f;
     }
 
 private:
@@ -118,13 +95,12 @@ private:
     {
         // calculate the new Front vector
         glm::vec3 front;
-        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        front.z = sin(glm::radians(-Pitch));
-        front.y = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.x = cos(glm::radians(YAW)) * cos(glm::radians(PITCH));
+        front.z = sin(glm::radians(-PITCH));
+        front.y = sin(glm::radians(YAW)) * cos(glm::radians(PITCH));
         Front = glm::normalize(front);
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         Up = glm::normalize(glm::cross(Right, Front));
     }
 };
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,17 @@ ModMeta __stdcall GetModInfo() {
     static ModMeta meta = {
         "Free Camera",
         "FreeCam",
-        "1.0.0",
-        "Kapilarny"
+        "1.1.0",
+        "Kapilarny & Kojo Bailey"
     };
 
     return meta;
 }
 
-std::string to_hex_str(__int64 num) {
-    std::stringstream stream;
-    stream << std::hex << num;
-    return stream.str();
+std::string char_to_string(char c) {
+    std::string s;
+    s.push_back(c);
+    return s;
 }
 
 struct mat4x4 {
@@ -32,75 +32,17 @@ struct vec3f {
     float x, y, z;
 };
 
-// 772910
-// __int64 __fastcall sub_772910(__int64 a1, vec3f* a2)
-typedef __int64(__fastcall* ASBR_Camera_SetPos)(__int64 a1, vec3f* a2);
-ASBR_Camera_SetPos ASBR_Camera_SetPos_Original;
-
-__int64 __fastcall ASBR_Camera_SetPos_Hook(__int64 a1, vec3f* a2)
-{
-    JAPI_LogInfo("ASBR_Camera_SetPos(" + std::to_string(a1) + ", [" + std::to_string(a2->x) + ", " + std::to_string(a2->y) + ", " + std::to_string(a2->z) + "])");
-
-    return ASBR_Camera_SetPos_Original(a1, a2);
-}
+struct {
+    bool left = false;
+    bool right = false;
+    bool forward = false;
+    bool backward = false;
+    bool up = false;
+    bool down = false;
+} KeyPressed;
 
 vec3f free_cam_pos = {69, 69, 69};
 vec3f free_cam_rot = {0, 0, 0};
-// vec3f LockedLookAt = {220, -915, 162};
-
-// 7727E0
-// double __fastcall ASBR_Camera_Update(__int64 a1)
-typedef double(__fastcall* ASBR_Camera_Update)(__int64 a1);
-ASBR_Camera_Update ASBR_Camera_Update_Original;
-
-double __fastcall ASBR_Camera_Update_Hook(__int64 a1)
-{
-    // Get the camera position (at a1 + 112)
-    vec3f* pos = (vec3f*)(a1 + 112);
-    vec3f* lookAt = (vec3f*)(a1 + 124);
-    // vec3f* tilt = (vec3f*)(a1 + 148);
-
-    // Grab the matrix
-    mat4x4* matrix = (mat4x4*)(a1 + 0x30);
-
-    if(pos->x != 640 || pos->y != 360 || pos->z != -320) {
-        pos->x = free_cam_pos.x;
-        pos->y = free_cam_pos.y;
-        pos->z = free_cam_pos.z;
-
-        // Set the look at forward
-        lookAt->x = pos->x + free_cam_rot.x; // x
-        lookAt->y = pos->y + 50; // z
-        lookAt->z = pos->z + free_cam_rot.y; // y
-
-        // Remove tilt
-        matrix->m[0][0] = 1;
-        matrix->m[1][0] = 0;
-        matrix->m[0][2] = 0;
-        matrix->m[1][2] = 1;
-
-        JAPI_LogInfo("ASBR_Camera_Update(" + to_hex_str(a1) + ") -> [" + std::to_string(pos->x) + ", " + std::to_string(pos->y) + ", " + std::to_string(pos->z) + "]");
-    }
-
-    // JAPI_LogInfo("ASBR_Camera_Update(" + std::to_string(a1) + ")");
-
-    return ASBR_Camera_Update_Original(a1);
-}
-
-// 772930
-// __int64 __fastcall ASBR_Camera_SetLookAt(__int64 a1, __int64 a2)
-typedef __int64(__fastcall* ASBR_Camera_SetLookAt)(__int64 a1, vec3f* a2);
-ASBR_Camera_SetLookAt ASBR_Camera_SetLookAt_Original;
-
-__int64 __fastcall ASBR_Camera_SetLookAt_Hook(__int64 a1, vec3f* a2)
-{
-    JAPI_LogInfo("ASBR_Camera_SetLookAt(" + to_hex_str(a1) + ", [" + std::to_string(a2->x) + ", " + std::to_string(a2->y) + ", " + std::to_string(a2->z) + "])");
-
-    return ASBR_Camera_SetLookAt_Original(a1, a2);
-}
-
-// 66185C
-// LRESULT __fastcall ASBR_HWNDProcCallback(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 typedef LRESULT(__fastcall* ASBR_HWNDProcCallback)(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 ASBR_HWNDProcCallback ASBR_HWNDProcCallback_Original;
@@ -111,23 +53,24 @@ static int res_width = 0;
 static int res_height = 0;
 static bool disable_ui = false;
 
-static char FORWARD_KEY = 'I';
-static char BACKWARD_KEY = 'K';
-static char LEFT_KEY = 'J';
-static char RIGHT_KEY = 'L';
+static char* LEFT_KEY = new char[255];
+static char* RIGHT_KEY = new char[255];
+static char* FORWARD_KEY = new char[255];
+static char* BACKWARD_KEY = new char[255];
+static char* UP_KEY = new char[255];
+static char* DOWN_KEY = new char[255];
+static char* BLOCK_CAMERA_KEY = new char[255];
 
 bool free_cam_enabled = false;
+bool movement_blocked = false;
 
-LRESULT __fastcall ASBR_HWNDProcCallback_Hook(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    constexpr float speed = 20;
-
+LRESULT __fastcall ASBR_HWNDProcCallback_Hook(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     // if key is pressed, toggle free cam
-    if(msg == WM_KEYUP && wParam == VK_F5) {
+    if (msg == WM_KEYUP && wParam == VK_F5) {
         free_cam_enabled = !free_cam_enabled;
         JAPI_LogInfo("Free cam: " + std::to_string(free_cam_enabled));
     
-        if(free_cam_enabled) {
+        if (free_cam_enabled) {
             // Make the cursor invisible
             ShowCursor(FALSE);
 
@@ -144,25 +87,40 @@ LRESULT __fastcall ASBR_HWNDProcCallback_Hook(HWND hWnd, UINT msg, WPARAM wParam
         }
     }
 
-    if(!free_cam_enabled) {
+    if (!free_cam_enabled) {
         ShowCursor(TRUE);
         return ASBR_HWNDProcCallback_Original(hWnd, msg, wParam, lParam);
     }
 
-    // If I/J/K/L are pressed, move the camera
-    if(msg == WM_KEYDOWN) {
-        if(wParam == 'I') {
-            camera.ProcessKeyboard(FORWARD, speed);
-        } else if(wParam == 'K') {
-            camera.ProcessKeyboard(BACKWARD, speed);
-        } else if(wParam == 'J') {
-            camera.ProcessKeyboard(LEFT, speed);
-        } else if(wParam == 'L') {
-            camera.ProcessKeyboard(RIGHT, speed);
-        }
+    if(msg == WM_KEYDOWN && wParam == BLOCK_CAMERA_KEY[0]) {
+        movement_blocked = !movement_blocked;
     }
 
-    if(msg == WM_MOUSEMOVE) {
+    if(movement_blocked) {
+        ShowCursor(TRUE);
+        return ASBR_HWNDProcCallback_Original(hWnd, msg, wParam, lParam);
+    }
+
+    // Record camera movement inputs.
+    if (msg == WM_KEYDOWN) {
+        if (wParam == RIGHT_KEY[0])    KeyPressed.right = true;
+        if (wParam == LEFT_KEY[0])     KeyPressed.left = true;
+        if (wParam == FORWARD_KEY[0])  KeyPressed.forward = true;
+        if (wParam == BACKWARD_KEY[0]) KeyPressed.backward = true;
+        if (wParam == UP_KEY[0])    KeyPressed.right = true;
+        if (wParam == DOWN_KEY[0])     KeyPressed.left = true;
+    }
+
+    if (msg == WM_KEYUP) {
+        if (wParam == RIGHT_KEY[0])    KeyPressed.right = false;
+        if (wParam == LEFT_KEY[0])     KeyPressed.left = false;
+        if (wParam == FORWARD_KEY[0])  KeyPressed.forward = false;
+        if (wParam == BACKWARD_KEY[0]) KeyPressed.backward = false;
+        if (wParam == UP_KEY[0])    KeyPressed.right = false;
+        if (wParam == DOWN_KEY[0])     KeyPressed.left = false;
+    }
+
+    if (msg == WM_MOUSEMOVE) {
         // Get the mouse position
         int x = LOWORD(lParam);
         int y = HIWORD(lParam);
@@ -190,37 +148,8 @@ LRESULT __fastcall ASBR_HWNDProcCallback_Hook(HWND hWnd, UINT msg, WPARAM wParam
         int diffY = y - p.y;
 
         // Rotate the camera
-        // JAPI_LogInfo("Mouse moved: " + std::to_string(diffX) + ", " + std::to_string(diffY));
         camera.ProcessMouseMovement(-diffX, diffY);
     }
-
-    // // If mouse moved rotate the camera
-    // if(msg == WM_MOUSEMOVE) {
-    //     // Get the mouse position
-    //     int x = LOWORD(lParam);
-    //     int y = HIWORD(lParam);
-
-    //     // Get the window size
-    //     RECT rect;
-    //     GetClientRect(hWnd, &rect);
-
-    //     // Calculate the center of the window
-    //     int centerX = rect.left + (rect.right - rect.left) / 2;
-    //     int centerY = rect.top + (rect.bottom - rect.top) / 2;
-
-    //     // Calculate the difference
-    //     int diffX = x - centerX;
-    //     int diffY = y - centerY;
-
-    //     // Set the mouse position to the center
-    //     SetCursorPos(centerX, centerY);
-
-
-
-    //     // Rotate the camera
-    //     free_cam_rot.y += diffX * 10.0f;
-    //     free_cam_rot.x += diffY * 10.0f;
-    // }
 
     auto result = ASBR_HWNDProcCallback_Original(hWnd, msg, wParam, lParam);
     ShowCursor(FALSE); // Make the cursor invisible
@@ -228,49 +157,41 @@ LRESULT __fastcall ASBR_HWNDProcCallback_Hook(HWND hWnd, UINT msg, WPARAM wParam
     return result;
 }
 
-// Sample Matrix
-// 0.578564 -0.815623 0.004762 -99.157143
-// -0.027465 -0.013646 0.999530 -147.159210
-// -0.815175 -0.578422 -0.030296 -712.216980
-// 0.000000 0.000000 0.000000 1.000000
-
 void copy_mat4x4(mat4x4* dest, glm::mat4* src) {
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             dest->m[i][j] = (*src)[i][j];
         }
     }
 }
 
-// 6C9300
-// _DWORD *__fastcall ASBR_CopyMatrix4x4Inversed(_DWORD *to, _DWORD *from)
 typedef void*(__fastcall* ASBR_CopyMatrix4x4Inversed)(void* to, void* from);
 ASBR_CopyMatrix4x4Inversed ASBR_CopyMatrix4x4Inversed_Original;
 
-void* __fastcall ASBR_CopyMatrix4x4Inversed_Hook(void* to, void* from)
-{
+void* __fastcall ASBR_CopyMatrix4x4Inversed_Hook(void* to, void* from) {
     return ASBR_CopyMatrix4x4Inversed_Original(to, from);
 }
 
-// 7727E0
-// void* __fastcall sub_7727E0(__int64 a1)
 typedef mat4x4*(__fastcall* ASBR_CreateLookAtMatrix)(__int64 a1);
 ASBR_CreateLookAtMatrix ASBR_CreateLookAtMatrix_Original;
 
-mat4x4* __fastcall ASBR_CreateLookAtMatrix_Hook(__int64 a1)
-{
+mat4x4* __fastcall ASBR_CreateLookAtMatrix_Hook(__int64 a1) {
     vec3f* pos = (vec3f*)(a1 + 112);
     mat4x4* matrix = (mat4x4*)(a1 + 0x30);
 
-    // JAPI_LogInfo("ASBR_CreateLookAtMatrix(" + to_hex_str(a1) + ") -> [" + std::to_string(pos->x) + ", " + std::to_string(pos->y) + ", " + std::to_string(pos->z) + "]");
+    // If free cam isn't currently active...
+    if (!free_cam_enabled)
+        return ASBR_CreateLookAtMatrix_Original(a1);
 
-    if(!free_cam_enabled) return ASBR_CreateLookAtMatrix_Original(a1);
-
-    if(res_width && res_height && pos->x == res_width / 2 && pos->y == res_height / 2 && pos->z == -320) {
-        // JAPI_LogInfo("UI Camera Call: " + std::to_string(pos->x) + ", " + std::to_string(pos->y) + ", " + std::to_string(pos->z));
-
+    // If is HUD camera, determined based on its consistent position...
+    if (res_width && res_height && pos->x == res_width / 2 && pos->y == res_height / 2 && pos->z == -320) {
         return ASBR_CreateLookAtMatrix_Original(a1);
     }
+
+    // Move the camera with corresponding inputs.
+    // camera.ChangeX(KeyPressed.right - KeyPressed.left);
+    camera.ChangeZ(KeyPressed.forward - KeyPressed.backward);
+    camera.ChangeY(KeyPressed.up - KeyPressed.down);
 
     ASBR_CreateLookAtMatrix_Original(a1);
 
@@ -283,27 +204,6 @@ mat4x4* __fastcall ASBR_CreateLookAtMatrix_Hook(__int64 a1)
 
     return matrix;
 }
-
-Hook hook_ASBR_Camera_SetPos = {
-    (void*)0x772910,
-    (void*)ASBR_Camera_SetPos_Hook,
-    (void**)&ASBR_Camera_SetPos_Original,
-    "ASBR_Camera_SetPos"
-};
-
-Hook hook_ASBR_Camera_Update = {
-    (void*)0x7727E0,
-    (void*)ASBR_Camera_Update_Hook,
-    (void**)&ASBR_Camera_Update_Original,
-    "ASBR_Camera_Update"
-};
-
-Hook hook_ASBR_Camera_SetLookAt = {
-    (void*)0x772930,
-    (void*)ASBR_Camera_SetLookAt_Hook,
-    (void**)&ASBR_Camera_SetLookAt_Original,
-    "ASBR_Camera_SetLookAt"
-};
 
 Hook hook_ASBR_HWNDProcCallback = {
     (void*)0x66185C,
@@ -326,37 +226,34 @@ Hook hook_ASBR_CopyMatrix4x4Inversed = {
     "ASBR_CopyMatrix4x4Inversed"
 };
 
-std::string char_to_string(char c) {
-    std::string s;
-    s.push_back(c);
-    return s;
-}
-
 void __stdcall ModInit() {
     JAPI_LogInfo("BUILD: " __DATE__ " " __TIME__);
 
-    // // MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY)
-    SPEED = JAPI_ConfigBindFloat("MovementSpeed", SPEED);
-    SENSITIVITY = JAPI_ConfigBindFloat("MouseSensitivity", SENSITIVITY);
+    // Load configurations
+        // Camera properties
+        JAPI_ConfigRegisterFloat(&SPEED, "MovementSpeed", SPEED);
+        JAPI_ConfigRegisterFloat(&SENSITIVITY, "MouseSensitivity", SENSITIVITY);
 
-    res_width = JAPI_ConfigBindInt("ResolutionWidth", 0);
-    res_height = JAPI_ConfigBindInt("ResolutionHeight", 0);
+        // Target resolution
+        JAPI_ConfigRegisterInt(&res_width, "ResolutionWidth", res_width);
+        JAPI_ConfigRegisterInt(&res_height, "ResolutionHeight", res_height);
 
-    FORWARD_KEY = JAPI_ConfigBindString("KeyForward", char_to_string(FORWARD_KEY)).at(0);
-    BACKWARD_KEY = JAPI_ConfigBindString("KeyBackward", char_to_string(BACKWARD_KEY)).at(0);
-    LEFT_KEY = JAPI_ConfigBindString("KeyLeft", char_to_string(LEFT_KEY)).at(0);
-    RIGHT_KEY = JAPI_ConfigBindString("KeyRight", char_to_string(RIGHT_KEY)).at(0);
+        // Camera input keys
+        JAPI_ConfigRegisterString(LEFT_KEY, "KeyLeft", "J");
+        JAPI_ConfigRegisterString(RIGHT_KEY, "KeyRight", "L");
+        JAPI_ConfigRegisterString(FORWARD_KEY, "KeyForward", "I");
+        JAPI_ConfigRegisterString(BACKWARD_KEY, "KeyBackward", "K");
+        JAPI_ConfigRegisterString(UP_KEY, "KeyUp", "U");
+        JAPI_ConfigRegisterString(DOWN_KEY, "KeyDown", "O");
+        JAPI_ConfigRegisterString(BLOCK_CAMERA_KEY, "BlockCameraKey", "B");
 
-    if(res_width != 0 && res_height != 0) {
-        JAPI_LogInfo("Resolution Set: " + std::to_string(res_width) + "x" + std::to_string(res_height));
+    if (res_width && res_height) {
+        JAPI_LogInfo("Resolution specified: " + std::to_string(res_width) + "x" + std::to_string(res_height));
     } else {
-        JAPI_LogWarn("Resolution not set! UI may not work correctly!");
+        JAPI_LogWarn("Resolution not specified! HUD may not work correctly.");
     }
 
-    if(0
-        // || !JAPI_HookASBRFunction(&hook_ASBR_Camera_SetPos)
-        // || !JAPI_HookASBRFunction(&hook_ASBR_Camera_Update)
-        // || !JAPI_HookASBRFunction(&hook_ASBR_Camera_SetLookAt)
+    if (0
         || !JAPI_HookASBRFunction(&hook_ASBR_HWNDProcCallback)
         || !JAPI_HookASBRFunction(&hook_ASBR_CreateLookAtMatrix)
         || !JAPI_HookASBRFunction(&hook_ASBR_CopyMatrix4x4Inversed)
@@ -364,6 +261,5 @@ void __stdcall ModInit() {
         JAPI_LogError("Failed to apply patches!");
         return;
     }
-
     JAPI_LogInfo("Applied patches!");
 }
